bc9ee7106c53ef79092d7c0adf242a1eb69fcf6b
Shahinkhalajestani <vatharishok@gmail.com>
2022-01-02 18:16:30 +0330

    load balancing with api-gateway and eureka discovery.

--- resource-server/src/main/java/com/shahintraining/resourceserver/controller/UsersController.java
+++ resource-server/src/main/java/com/shahintraining/resourceserver/controller/UsersController.java
@@ -1,6 +1,8 @@
 package com.shahintraining.resourceserver.controller;
 
 import com.shahintraining.resourceserver.domain.UserDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -18,24 +20,27 @@ import org.springframework.web.bind.annotation.*;
 public class UsersController {
 
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping(value = "/status/check")
-    public String status(){
-        return "working ... ";
+    public String status() {
+        return "working ... " + environment.getProperty("local.server.port");
     }
 
-//    @Secured("ROLE_developer")
+    //    @Secured("ROLE_developer")
     @PreAuthorize("hasRole('developer') or #userId== #jwt.subject")
     @DeleteMapping(value = "/delete/{userId}")
-    public String deleteUser(@AuthenticationPrincipal Jwt jwt, @PathVariable("userId") String userId){
+    public String deleteUser(@AuthenticationPrincipal Jwt jwt, @PathVariable("userId") String userId) {
         String s = "shahin";
-        return String.format("the user with Id of %2$s,and Name of %1$s deleted",s,userId);
+        return String.format("the user with Id of %2$s,and Name of %1$s deleted", s, userId);
     }
 
 
     //don't use postauthorize for methods that do something to data it can be used for getting data or so
     @GetMapping(value = "/{userId}")
     @PostAuthorize("returnObject.id == #jwt.subject")
-    public UserDto getUser(@AuthenticationPrincipal Jwt jwt,@PathVariable("userId") String userId){
-        return new UserDto("shahin","khalajestani",userId);
+    public UserDto getUser(@AuthenticationPrincipal Jwt jwt, @PathVariable("userId") String userId) {
+        return new UserDto("shahin", "khalajestani", userId);
     }
 }
