c30c62967ee1148aa311eb7bd3720c701796c992
sh.khalajestanii <NiH@sh123>
2022-01-10 17:24:06 +0330

    Used Web Client Instead of rest Template.

--- photo-app-web-client/src/main/java/com/shahintraining/photoappwebclient/configuration/ApplicationConfiguration.java
+++ photo-app-web-client/src/main/java/com/shahintraining/photoappwebclient/configuration/ApplicationConfiguration.java
@@ -2,7 +2,11 @@ package com.shahintraining.photoappwebclient.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * @author sh.khalajestanii
@@ -15,6 +19,17 @@ public class ApplicationConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+
+    @Bean
+    public WebClient webClient(ClientRegistrationRepository clientRegistrationRepository,
+                               OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository) {
+        ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 =
+                new ServletOAuth2AuthorizedClientExchangeFilterFunction(clientRegistrationRepository,
+                        oAuth2AuthorizedClientRepository);
+        oauth2.setDefaultOAuth2AuthorizedClient(true);
+        return WebClient.builder().apply(oauth2.oauth2Configuration()).build();
     }
 
 }
--- photo-app-web-client/src/main/java/com/shahintraining/photoappwebclient/controllers/AlbumsController.java
+++ photo-app-web-client/src/main/java/com/shahintraining/photoappwebclient/controllers/AlbumsController.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.List;
 
@@ -30,44 +31,22 @@ public class AlbumsController {
     private OAuth2AuthorizedClientService oAuth2AuthorizedClientService;
 
     @Autowired
+    private WebClient webClient;
+
+    @Autowired
     private RestTemplate restTemplate;
 
     @GetMapping("/albums")
     public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
 
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        OAuth2AuthenticationToken authenticationToken = (OAuth2AuthenticationToken) authentication;
-
-        OAuth2AuthorizedClient oAuth2AuthorizedClient = oAuth2AuthorizedClientService.loadAuthorizedClient(authenticationToken.getAuthorizedClientRegistrationId(),
-                authenticationToken.getName());
-
-        String jwtAccessToken = oAuth2AuthorizedClient.getAccessToken().getTokenValue();
-
-        System.out.println("AccessToken Is = " + jwtAccessToken);
-
-
-        System.out.println("Princibal =" + principal);
-
-        OidcIdToken idToken = principal.getIdToken();
-
-        String tokenValue = idToken.getTokenValue();
-
-        System.out.println("token value =" + tokenValue);
 
         String url = "http://localhost:8095/albums";
 
-        HttpHeaders headers = new HttpHeaders();
+        List<Album> albums = webClient.get().uri(url).retrieve()
+                .bodyToMono(new ParameterizedTypeReference<List<Album>>() {
+        }).block();
 
-        headers.set("Authorization","Bearer "+jwtAccessToken);
-        
-        HttpEntity<List<Album>> requestEntity = new HttpEntity<>(headers);
-
-
-        ResponseEntity<List<Album>> responseEntity = restTemplate
-                .exchange(url, HttpMethod.GET, requestEntity, new ParameterizedTypeReference<List<Album>>() {
-        });
-        model.addAttribute("albums", responseEntity.getBody());
+        model.addAttribute("albums", albums);
         return "albums";
     }
 
