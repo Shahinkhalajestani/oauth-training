6fb8aa70492224ba9ffcb5297a09e678bc3c3d0f
Shahinkhalajestani <vatharishok@gmail.com>
2022-01-13 10:13:37 +0330

    Added props To authenticate with external servers.

---
+++ social-login-web-client/src/main/java/com/shahintraining/socialloginwebclient/security/WebSecurity.java
@@ -0,0 +1,19 @@
+package com.shahintraining.socialloginwebclient.security;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated()
+                .and().oauth2Login();
+    }
+}
