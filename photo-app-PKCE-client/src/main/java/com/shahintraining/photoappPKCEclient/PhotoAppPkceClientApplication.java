650b9a82367e9ed9051b2c295c6b87e9c7bdf1b4
Shahinkhalajestani <vatharishok@gmail.com>
2022-01-20 13:41:17 +0330

    Added Photo App PKCE client .

---
+++ photo-app-PKCE-client/src/main/java/com/shahintraining/photoappPKCEclient/PhotoAppPkceClientApplication.java
@@ -0,0 +1,13 @@
+package com.shahintraining.photoappPKCEclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoAppPkceClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoAppPkceClientApplication.class, args);
+	}
+
+}
---
+++ photo-app-PKCE-client/src/test/java/com/shahintraining/photoappPKCEclient/PhotoAppPkceClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahintraining.photoappPKCEclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppPkceClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
