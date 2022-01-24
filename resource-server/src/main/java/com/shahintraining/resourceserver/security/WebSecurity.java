083796b6178ecc67e794f0714843af930c43127b
Shahinkhalajestani <vatharishok@gmail.com>
2022-01-24 22:07:35 +0330

    Get to a resource directly CORS (without gateway)

--- resource-server/src/main/java/com/shahintraining/resourceserver/security/WebSecurity.java
+++ resource-server/src/main/java/com/shahintraining/resourceserver/security/WebSecurity.java
@@ -1,5 +1,6 @@
 package com.shahintraining.resourceserver.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,6 +8,12 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author sh.khalajestanii
@@ -14,14 +21,14 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
  * @since 12/15/2021
  */
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(securedEnabled = true,prePostEnabled = true)
+@EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true)
 public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
         jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(new KeyCloakRoleConverter());
-        http.
-                authorizeRequests()
+        http.cors().and()
+                .authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/users/status/check")
                 // with authorities, we need the prefix with roles we don't
 //                    .hasAnyAuthority("SCOPE_profile")
@@ -34,4 +41,17 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .jwt()
                 .jwtAuthenticationConverter(jwtAuthenticationConverter);
     }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowedOrigins(List.of("*"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
 }
